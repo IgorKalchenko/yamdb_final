096629ad7fedb8849626c3a7a6f55013ae19e3af
Игорь Кальченко <igorkalchenko1997@gmail.com>
2022-07-06 11:45:18 +0300

    minor fixes

--- api_yamdb/api/management/commands/import_data.py
+++ api_yamdb/api/management/commands/import_data.py
@@ -42,4 +42,4 @@ class Command(BaseCommand):
             self.stdout.write(
                 f'Выполнен импорт данных для таблицы {model.__name__}.'
             )
-        return
+        return f'Выполнен импорт данных для таблицы {model.__name__}.'
